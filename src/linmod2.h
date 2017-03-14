f0634f6eae3975e95d45d69ee0f6a8a8ce761799
Drew Schmidt <wrathematics@gmail.com>
2017-03-14 18:20:08 -0400

    main header

---
+++ src/linmod2.h
@@ -0,0 +1,14 @@
+#ifndef LINMOD2_H_
+#define LINMOD2_H_
+
+
+#include "include/cdefs.h"
+#include "include/types.h"
+
+#include "include/RNACI.h"
+#include "include/reactor.h"
+
+#include "include/lapack.h"
+
+
+#endif
