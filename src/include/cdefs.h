37ea7848af1a9e2acbd264b23609cbceecbb9341
Drew Schmidt <wrathematics@gmail.com>
2017-03-14 18:19:24 -0400

    common defs

---
+++ src/include/cdefs.h
@@ -0,0 +1,17 @@
+#ifndef LINMOD2_CDEFS_H_
+#define LINMOD2_CDEFS_H_
+
+
+#define MIN(a,b) ((a)<(b)?(a):(b))
+#define MAX(a,b) ((a)>(b)?(a):(b))
+
+#define THROW_MEMERR error("unable to allocate necessary memory")
+#define CHECKMALLOC(ptr) if (ptr == NULL) return -1;
+
+#define THROW_LAPACKERR(info) error("LAPACK returned error code %d", info)
+#define CHECKINFO(info) if (info!=0) THROW_LAPACKERR(info)
+
+#define FREE(ptr) if(ptr!=NULL) free(ptr)
+
+
+#endif
