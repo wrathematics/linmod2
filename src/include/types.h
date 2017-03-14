284722f5ff05c7191a8f34196a973711a7b104d6
Drew Schmidt <wrathematics@gmail.com>
2017-03-14 18:19:14 -0400

    use type shorthand

---
+++ src/include/types.h
@@ -0,0 +1,22 @@
+#ifndef LINMOD2_TYPES_H_
+#define LINMOD2_TYPES_H_
+
+
+#include <stdbool.h>
+
+typedef const bool    cbool;
+typedef const char    cchar;
+typedef const int     cint;
+typedef const double  cdbl;
+
+typedef char *const restrict char_r;
+typedef const char *const restrict cchar_r;
+
+typedef int *const restrict int_r;
+typedef const int *const restrict cint_r;
+
+typedef double *const restrict dbl_r;
+typedef const double *const restrict cdbl_r;
+
+
+#endif
