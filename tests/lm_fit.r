acdf58d33c75cc20556ecc6b88619b461df6c9f0
Drew Schmidt <wrathematics@gmail.com>
2017-03-21 18:27:46 -0400

    finish m>n tests for lm

--- tests/lm_fit.r
+++ tests/lm_fit.r
@@ -1,14 +1,18 @@
 library(linmod2)
 set.seed(1234)
 
+cmp <- function(a, b) stopifnot(all.equal(as.matrix(a), b))
+
 m = 20
 n = 5
 x = matrix(rnorm(m*n), m, n)
 y = rnorm(m)
 
+
+
 mdl1 = .lm.fit(x, y)
 mdl2 = .lm_fit(x, y)
 
-
-# stopifnot(all.equal(as.matrix(mdl1$residuals), mdl2$residuals))
-stopifnot(all.equal(as.matrix(mdl1$coefficients), mdl2$coefficients))
+cmp(mdl1$coefficients, mdl2$coefficients)
+cmp(mdl1$residuals, mdl2$residuals)
+cmp(x %*% mdl1$coefficients, mdl2$fitted.values)
