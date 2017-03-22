a799616fa41ff29ee19756354065068d5b8148a5
Drew Schmidt <wrathematics@gmail.com>
2017-03-21 22:46:58 -0400

    better benchmark

--- inst/benchmarks/lm_fit.r
+++ inst/benchmarks/lm_fit.r
@@ -1,13 +1,16 @@
 library(linmod2)
 library(rbenchmark)
+set.seed(1234)
 
+cols = cols <- c("test", "replications", "elapsed", "relative")
+reps = 25
 
 m = 2000
 n = 500
 x = matrix(rnorm(m*n), m, n)
 y = rnorm(m)
 
-benchmark(.lm.fit(x, y), .lm_fit(x, y), replications=25)
+benchmark(.lm.fit(x, y), .lm_fit(x, y), replications=reps, columns=cols)
 
 
 
@@ -16,4 +19,4 @@ n = 2000
 x = matrix(rnorm(m*n), m, n)
 y = rnorm(m)
 
-benchmark(.lm.fit(x, y), .lm_fit(x, y), replications=25)
+benchmark(.lm.fit(x, y), .lm_fit(x, y), replications=reps, columns=cols)
