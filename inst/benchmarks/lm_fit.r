02e72e718e6ae63b97f21b3c2af18ba0952063a5
Drew Schmidt <wrathematics@gmail.com>
2017-05-16 11:30:05 -0400

    add .lm_fit_minimal to benchmarks

--- inst/benchmarks/lm_fit.r
+++ inst/benchmarks/lm_fit.r
@@ -10,7 +10,7 @@ n = 500
 x = matrix(rnorm(m*n), m, n)
 y = rnorm(m)
 
-benchmark(.lm.fit(x, y), .lm_fit(x, y), replications=reps, columns=cols)
+benchmark(.lm.fit(x, y), .lm_fit(x, y), .lm_fit_minimal(x, y), replications=reps, columns=cols)
 
 
 
@@ -19,4 +19,4 @@ n = 2000
 x = matrix(rnorm(m*n), m, n)
 y = rnorm(m)
 
-benchmark(.lm.fit(x, y), .lm_fit(x, y), replications=reps, columns=cols)
+benchmark(.lm.fit(x, y), .lm_fit(x, y), .lm_fit_minimal(x, y), replications=reps, columns=cols)
