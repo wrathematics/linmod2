e5fb763cf0a14a0819412abab20f586fa0757c01
Drew Schmidt <wrathematics@gmail.com>
2017-05-16 10:14:05 -0400

    include internals in linmod2.h, add error number defs

--- src/include/linmod2/linmod2.h
+++ src/include/linmod2/linmod2.h
@@ -2,6 +2,15 @@
 #define __LINMOD2_H__
 
 
+#define LINMOD2_OK 0
+
+#define LINMOD2_ERR_MALLOC -1
+#define LINMOD2_ERR_LAPACK -2
+
+#include "src/internal/cdefs.h"
+#include "src/internal/lapack.h"
+#include "src/internal/types.h"
+
 #include "src/lm.h"
 
 
