ad16f0442af686c5428325ebc46c4ecb5256dd08
Drew Schmidt <wrathematics@gmail.com>
2017-05-16 11:30:24 -0400

    add larger (too big for cache) benchmark

---
+++ inst/benchmarks/big_lm_fit.r
@@ -0,0 +1,15 @@
+library(linmod2)
+library(rbenchmark)
+set.seed(1234)
+
+cols = cols <- c("test", "replications", "elapsed", "relative")
+reps = 5
+
+m = 10000
+n = 500
+x = matrix(rnorm(m*n), m, n)
+y = rnorm(m)
+
+
+#benchmark(.lm.fit(x, y), .lm_fit(x, y), .lm_fit_minimal(x, y), replications=reps, columns=cols)
+benchmark(.lm.fit(x, y), .lm_fit(x, y), replications=reps, columns=cols)
