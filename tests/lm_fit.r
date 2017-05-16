dfc0fffc7a81b509cc88ae81ae986e261f756908
Drew Schmidt <wrathematics@gmail.com>
2017-05-16 11:30:44 -0400

    better tests

--- tests/lm_fit.r
+++ tests/lm_fit.r
@@ -2,6 +2,11 @@ library(linmod2)
 set.seed(1234)
 
 cmp <- function(a, b) stopifnot(all.equal(as.matrix(a), b))
+fit <- function(x, y)
+{
+  mdl1 <<- .lm.fit(x, y)
+  mdl2 <<- .lm_fit_minimal(x, y)
+}
 
 m = 20
 n = 5
@@ -9,19 +14,19 @@ x = matrix(rnorm(m*n), m, n)
 y = rnorm(m)
 
 
+# ---------------- Tall and skinny ----------------
 
-mdl1 = .lm.fit(x, y)
-mdl2 = .lm_fit_minimal(x, y)
+# 1 RHS
+fit(x, y)
 
 cmp(mdl1$coefficients, mdl2$coefficients)
 cmp(x %*% mdl1$coefficients, mdl2$fitted.values)
 cmp(mdl1$residuals, mdl2$residuals)
 
 
-
+# 3 RHS
 y = matrix(rnorm(m*3), m, 3)
-mdl1 = .lm.fit(x, y)
-mdl2 = .lm_fit_minimal(x, y)
+fit(x, y)
 
 cmp(mdl1$coefficients, mdl2$coefficients)
 cmp(x %*% mdl1$coefficients, mdl2$fitted.values)
@@ -29,13 +34,24 @@ cmp(mdl1$residuals, mdl2$residuals)
 
 
 
-x = t(x)
-y = y[1:n]
-mdl1 = .lm.fit(x, y)
-mdl2 = .lm_fit_minimal(x, y)
+# ---------------- Short and wide ----------------
 
 #### NOTE: the coefficients can't possibly be equal because R will fit a model
-#### with only the first `rank==m` coefficients, while we use all `n`.
-### cmp(mdl1$coefficients, mdl2$coefficients)
+#### with only the first 'rank==m' coefficients, while we use all 'n'.
+
+# 1 RHS
+x = t(x)
+y = y[1:n]
+fit(x, y)
+
+cmp(mdl1$residuals, mdl2$residuals)
+cmp(x %*% mdl1$coefficients, mdl2$fitted.values)
+
+
+# 3 RHS
+
+y = matrix(rnorm(n*3), n, 3)
+fit(x, y)
+
 cmp(mdl1$residuals, mdl2$residuals)
 cmp(x %*% mdl1$coefficients, mdl2$fitted.values)
