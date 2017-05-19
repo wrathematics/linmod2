5be4dc2c0860a867eae1e8dfa4c850b9a226be1a
Drew Schmidt <wrathematics@gmail.com>
2017-05-18 23:12:18 -0400

    more benchmarks

---
+++ inst/benchmarks/dot_lm_fit.r
@@ -0,0 +1,22 @@
+library(linmod2)
+library(rbenchmark)
+set.seed(1234)
+
+cols = cols <- c("test", "replications", "elapsed", "relative")
+reps = 25
+
+m = 2000
+n = 500
+x = matrix(rnorm(m*n), m, n)
+y = rnorm(m)
+
+benchmark(.lm.fit(x, y), .lm_fit(x, y), .lm_fit_minimal(x, y), replications=reps, columns=cols)
+
+
+
+m = 500
+n = 2000
+x = matrix(rnorm(m*n), m, n)
+y = rnorm(m)
+
+benchmark(.lm.fit(x, y), .lm_fit(x, y), .lm_fit_minimal(x, y), replications=reps, columns=cols)
