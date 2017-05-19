b13a8babb18f2b1fb864bb2b234642c4352ee124
Drew Schmidt <wrathematics@gmail.com>
2017-05-18 23:01:57 -0400

    add native bindings

--- src/linmod2_native.c
+++ src/linmod2_native.c
@@ -1,16 +1,18 @@
 /* Automatically generated. Do not edit by hand. */
-  
-  #include <R.h>
-  #include <Rinternals.h>
-  #include <R_ext/Rdynload.h>
-  #include <stdlib.h>
+
+#include <R.h>
+#include <Rinternals.h>
+#include <R_ext/Rdynload.h>
+#include <stdlib.h>
 
 extern SEXP R_dot_lm_fit_minimal(SEXP x, SEXP y, SEXP intercept);
 extern SEXP R_dot_lm_fit(SEXP x, SEXP y, SEXP intercept);
+extern SEXP R_lm_fit(SEXP x, SEXP y, SEXP intercept);
 
 static const R_CallMethodDef CallEntries[] = {
   {"R_dot_lm_fit_minimal", (DL_FUNC) &R_dot_lm_fit_minimal, 3},
   {"R_dot_lm_fit", (DL_FUNC) &R_dot_lm_fit, 3},
+  {"R_lm_fit", (DL_FUNC) &R_lm_fit, 3},
   {NULL, NULL, 0}
 };
 
